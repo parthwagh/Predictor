f5effc31f65cec23ea163a9b95d4e54af08872d2
Parth Waghmare <parthwaghmare@Siddhants-MacBook-Air.local>
2024-03-12 11:07:07 +0530

    predictor app nextjs

--- src/app/page.js
+++ src/app/page.js
@@ -1,94 +1,49 @@
+"use client"
 import Image from "next/image";
 import styles from "./page.module.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function Home() {
+  const [name, setName] = useState("");
+  const [name1, setName1] = useState("");
+  const [age, setAge] = useState("");
+  const [country, setCountry] = useState("");
+  const [gen, setGen] = useState("");
+  const increment = () => {
+    console.log(name);
+    axios.get(`https://api.agify.io?name=${name}`).then((res) => {
+      console.log(res.data);
+      setName1(res.data.name);
+      setAge(res.data?.age);
+    });
+    axios.get(`https://api.genderize.io?name=${name}`).then((res) => {
+      console.log(res.data);
+      setGen(res.data?.gender);
+    });
+    axios.get(`https://api.nationalize.io?name=${name}`).then((res) => {
+      console.log(res.data);
+      setCountry(res.data?.country[0]?.country_id);
+    });
+  };
+  useEffect(() => {
+    increment();
+  }, [name]);
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div>
+      <h1 className={styles.headerText}>Predictor</h1>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        className={styles.textInput}
+      />
+      <button onClick={() => increment} className={styles.predictBtn}>Predict</button>
+      <h4 className={styles.infoText}>Name: {name1}</h4>
+      <h4 className={styles.infoText}>Age: {age}</h4>
+      <h4 className={styles.infoText}>Gender: {gen}</h4>
+      <h4 className={styles.infoText}>Country: {country}</h4>
       </div>
     </main>
   );
